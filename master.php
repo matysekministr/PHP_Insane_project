64a7f92291d90ee80610aaff43c993fb76eb54ad
Maty <jezek.matyas@student.sposdk.cz>
2024-12-06 09:21:57 +0100

    aa

--- master.php
+++ master.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form Example</title>
+    <title>PHP Form Eple</title>
 </head>
 <body>
 
